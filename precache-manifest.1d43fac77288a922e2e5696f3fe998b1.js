ba3691539fd971de995035fea7872a286518b627
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-09-17 22:26:02 -0300

    Updates

---
+++ precache-manifest.1d43fac77288a922e2e5696f3fe998b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e4e36be78962ebeeb6e39b82f8dd70",
+    "url": "/npm-stats/index.html"
+  },
+  {
+    "revision": "bce8e232303fb7c2ed0a",
+    "url": "/npm-stats/static/css/main.1417bbf5.chunk.css"
+  },
+  {
+    "revision": "60cd9833aa20984da288",
+    "url": "/npm-stats/static/js/2.48673f6c.chunk.js"
+  },
+  {
+    "revision": "bce8e232303fb7c2ed0a",
+    "url": "/npm-stats/static/js/main.6dfdd101.chunk.js"
+  },
+  {
+    "revision": "f62f7dc542a5930d0c1a",
+    "url": "/npm-stats/static/js/runtime~main.9c71a7ed.js"
+  }
+]);
